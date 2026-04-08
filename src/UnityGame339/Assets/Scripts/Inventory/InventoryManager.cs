aede82fc1181d6e62973225b12bc7c6eaba1a906
SamanthaHall18 <samhall@chapman.edu>
2026-04-08 14:43:19 -0700

    made an ItemSlot class and updated Inventory Manager

--- src/UnityGame339/Assets/Scripts/Inventory/InventoryManager.cs
+++ src/UnityGame339/Assets/Scripts/Inventory/InventoryManager.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public GameObject InventoryMenu;
+    public ItemSlot[] itemSlot;
     
     public List<Item> items = new List<Item>(); // tracks owned item ids
 
-    private bool isActivated;
+    private bool isMenuActivated;
     
     void Update()
     {
         // Open Inventory with "Q"
-        if (Input.GetKeyDown(KeyCode.Q) && !isActivated)
+        if (Input.GetKeyDown(KeyCode.Q) && !isMenuActivated)
         {
             Time.timeScale = 0;
             InventoryMenu.SetActive(true);
-            isActivated = true;
+            isMenuActivated = true;
         }
         
-        else if (Input.GetKeyDown(KeyCode.Q) && isActivated)
+        else if (Input.GetKeyDown(KeyCode.Q) && isMenuActivated)
         {
             Time.timeScale = 1;
             InventoryMenu.SetActive(false);
-            isActivated = false;
+            isMenuActivated = false;
         }
     }
 
     public void AddItem(string itemName, int quantity, Sprite sprite, int price)
     {
         Debug.Log("itemName = " + itemName + " quantity = " + quantity + " itemSprite = " + sprite + " price = " + price);
+        
+        
     }
     
 }
---
+++ src/UnityGame339/Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    //===ITEM DATA===
+    public string itemName;
+    public int quantity;
+    public Sprite itemSprite;
+    public int price;
+    public bool isFull;
+    
+    //===ITEM SLOT===
+    [SerializeField]
+    private TMP_Text quantityText;
+    
+    [SerializeField]
+    private Image itemImage;
+
+    public void AddItem(string itemName, int quantity, Sprite itemsprite, int price)
+    {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.itemSprite = itemsprite;
+        this.price = price;
+        this.isFull = true;
+        
+        quantityText.text = quantity.ToString();
+        quantityText.enabled = true;
+        
+        itemImage.sprite = itemsprite;
+        itemImage.enabled = true;
+        
+        
+    }
+}
